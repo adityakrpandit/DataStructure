57a7d98017576715fdaf0d6b7c9fa4ab7304e132
Aditya Kumar Pandit <adityakumarpandit643@gmail.com>
2026-07-21 22:19:40 +0530

    prg2.c

--- UNIT 1/prg2.c
+++ UNIT 1/prg2.c
@@ -1,42 +1,23 @@
-/* Enrollment No: 92500527175 */
+/* Enrollment No. - 92500527175 */
+/* 2. Create an array of size 10, input values and display sum and average of all elements in the array.*/
 
-#include <stdio.h>
+
+#include<stdio.h>
 
 void main()
 {
-    int a[10], i, n, sum = 0;
-    float avg;
+          int a[20],i,n,sum=0;
+          float avg;
+          printf("enter the limit:");
+          scanf("%d",&n);
 
-    printf("Enter the limit : ");
-    scanf("%d", &n);
-
-    if(n > 10 || n <= 0)
-    {
-        printf("Invalid limit! Please enter a value between 1 and 10.");
-        return 0;
-    }
-
-    printf("\nEnter %d elements:\n", n);
-
-    for(i = 0; i < n; i++)
-    {
-        printf("Enter value %d: ", i + 1);
-        scanf("%d", &a[i]);
-
-        sum = sum + a[i];
-    }
-
-    printf("\nArray Elements are:\n");
-
-    for(i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-
-    avg = (float)sum / n;
-
-    printf("\n\nSum = %d", sum);
-    printf("\nAverage = %.2f", avg);
-
-    return 0;
+          for(i=0;i<n;i++)
+          {
+              printf("\n Enter the number %d:",i+1);
+              scanf("%d",&a[i]);
+              sum=sum+a[i];
+          }
+          avg=(float)sum/n;
+          printf("the addition is %d",sum);
+          printf("\nthe average is %f",avg);
 }
